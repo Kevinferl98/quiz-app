81ab0eb6d0b72228849df698611faaa6d229dac7
Kevin Ferlauto <kevinferl1998@gmail.com>
2025-07-17 17:37:45 +0200

    - Added logout button in QuizListPage - Setup routing

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,20 +1,35 @@
 import "./HomePage.css";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 export default function HomePage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/quizzes");
+  }
+
   return (
     <div className="container">
       <div className="card">
         <h1 className="title">Login</h1>
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <input
-            type="username"
+            type="text"
             placeholder="Username"
             className="input"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
           <input
             type="password"
             placeholder="Password"
             className="input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <button type="submit" className="button">
             Login
--- frontend/src/pages/QuizListPage.jsx
+++ frontend/src/pages/QuizListPage.jsx
@@ -1,7 +1,38 @@
+import "./QuizListPage.css";
+import {useNavigate} from "react-router-dom";
+
 export default function QuizListPage() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate("/");
+  };
+
+  const quizzes = [
+    { id: 1, title: "Lorem Ipsum" },
+    { id: 2, title: "Lorem Ipsum" },
+    { id: 3, title: "Lorem Ipsum" },
+  ];
+
   return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold mb-4">Available Quizzes</h1>
+    <div>
+      <div className="topbar">
+        <h1 className="topbar-title">Quiz App</h1>
+        <button onClick={handleLogout} className="logout-button">
+          Logout
+        </button>
+      </div>
+
+      <div className="page-content">
+        <h2 className="page-title">Available Quizzes</h2>
+        <ul className="quiz-list">
+          {quizzes.map((quiz) => (
+            <li key={quiz.id} className="quiz-item">
+              {quiz.title}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
