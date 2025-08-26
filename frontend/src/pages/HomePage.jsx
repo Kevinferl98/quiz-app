319cce75d0d65ab2ebcf588208fc54858eabbca7
Kevin Ferlauto <kevinferl1998@gmail.com>
2025-08-26 17:48:17 +0200

    Added new form for password update

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
 import "../styles/HomePage.css";
 import {useState} from "react";
 import {useNavigate} from "react-router-dom";
-import { signIn } from 'aws-amplify/auth'
+import { signIn, confirmSignIn } from 'aws-amplify/auth'
 
 export default function HomePage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
   const [loading, setLoading] = useState(false);
+  const [challenge, setChallenge] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -15,10 +17,16 @@ export default function HomePage() {
     setErrorMsg("");
     setLoading(true);
     try {
-      await signIn({
+      const { nextStep } = await signIn({
         username: username,
         password: password
       });
+      console.log(nextStep);
+      if (nextStep.signInStep === "CONFIRM_SIGN_IN_WITH_NEW_PASSWORD_REQUIRED") {
+        setChallenge(true);
+        return;
+      }
+
       navigate("/quizzes");
     } catch (err) {
       setErrorMsg(err.message || "Error during login");
@@ -27,11 +35,29 @@ export default function HomePage() {
     }
   };
 
+  const handleNewPasswordSubmit = async (e) => {
+    e.preventDefault();
+    setErrorMsg("");
+    setLoading(true);
+
+    try {
+      await confirmSignIn({
+        challengeResponse: newPassword
+      });
+      navigate("/quizzes");
+    } catch (err) {
+      setErrorMsg(err.message || "Error during password update");
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <div className="container">
       <div className="card">
         <h1 className="title">Login</h1>
-        <form className="form" onSubmit={handleSubmit}>
+        {!challenge ? (
+           <form className="form" onSubmit={handleSubmit}>
           <input
             type="text"
             placeholder="Username"
@@ -51,6 +77,21 @@ export default function HomePage() {
           </button>
           {errorMsg && <div className="error">{errorMsg}</div>}
         </form>
+        ) : (
+          <form className="form" onSubmit={handleNewPasswordSubmit}>
+            <input
+              type="password"
+              placeholder="New password"
+              className="input"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+            />
+            <button type="submit" className="button" disabled={loading}>
+              {loading ? "Updating..." : "Update password"}
+            </button>
+            {errorMsg && <div className="error">{errorMsg}</div>}
+          </form>
+        )}
       </div>
     </div>
   );
