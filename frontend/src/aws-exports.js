60f7148bab180f34d67a88203842b825bc2b3b89
Kevin Ferlauto <kevinferl1998@gmail.com>
2025-08-26 21:56:50 +0200

    Remove config

--- frontend/src/aws-exports.js
+++ frontend/src/aws-exports.js
@@ -3,8 +3,7 @@ const awsconfig = {
       Cognito: {
         region: "",
         userPoolId: "",
-        userPoolClientId: "",
-        authenticationFlowType: ""
+        userPoolClientId: ""
       }
     },
 }
