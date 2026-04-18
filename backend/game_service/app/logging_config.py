d1d59e2fba7c92203f09a7a75d4d2236b50ee8ff
Kevin Ferlauto <kevinferl1998@gmail.com>
2026-04-18 16:38:40 +0200

    Add JSON log formatter and update logging config

--- backend/game_service/app/logging_config.py
+++ backend/game_service/app/logging_config.py
@@ -1,27 +1,21 @@
 import logging
 import logging.config
 from app.config import config
+from app.logging.json_formatter import JsonFormatter
 
 LOGGING_CONFIG = {
     "version": 1,
     "disable_existing_loggers": False,
 
     "formatters": {
-        "default": {
-            "format": "[%(asctime)s] %(levelname)s in %(name)s: %(message)s"
-        },
-        "detailed": {
-            "format": (
-                "[%(asctime)s] %(levelname)s "
-                "[%(process)d:%(threadName)s] "
-                "%(name)s: %(message)s"
-            )
-        },
+        "json": {
+            "()": JsonFormatter,
+        }
     },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
-            "formatter": "default",
+            "formatter": "json",
             "level": config.LOG_LEVEL
         }
     },
@@ -29,6 +23,18 @@ LOGGING_CONFIG = {
     "root": {
         "level": config.LOG_LEVEL,
         "handlers": ["console"]
+    },
+
+    "loggers": {
+        "uvicorn": {
+            "level": "INFO"
+        },
+        "uvicorn.error": {
+            "level": "INFO"
+        },
+        "uvicorn.access": {
+            "level": "WARNING"
+        }
     }
 }
 
